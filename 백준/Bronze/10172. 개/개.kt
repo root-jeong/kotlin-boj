aa85954541a64ac037daeb7708b80c5cab19b9a7
root <44860713+root-jeong@users.noreply.github.com>
2023-02-17 00:35:00 +0900

    [Bronze V] Title: 개, Time: 84 ms, Memory: 12044 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.kt
@@ -0,0 +1,7 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    println("|\\_/|")
+    println("|q p|   /}")
+    println("( 0 )\"\"\"\\")
+    println("|\"^\"`    |")
+    println("||_/=\\\\__|")
+}
