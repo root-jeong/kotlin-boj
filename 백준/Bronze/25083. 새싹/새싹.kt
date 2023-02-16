40c254d13772ade36ff1c645e957f97e1ab04ab8
root <44860713+root-jeong@users.noreply.github.com>
2023-02-17 00:36:01 +0900

    [Bronze V] Title: 새싹, Time: 84 ms, Memory: 12048 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.kt
@@ -0,0 +1,8 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    println("         ,r'\"7")
+    println("r`-_   ,'  ,/")
+    println(" \\. \". L_r'")
+    println("   `~\\/")
+    println("      |")
+    println("      |")
+}
