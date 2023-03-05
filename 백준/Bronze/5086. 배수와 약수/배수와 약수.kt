9cada43cf62a4e8bf5c273cf86afd1eb68e8d738
root <44860713+root-jeong@users.noreply.github.com>
2023-03-05 23:08:51 +0900

    [Bronze III] Title: 배수와 약수, Time: 124 ms, Memory: 20460 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.kt
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.kt
@@ -6,20 +6,13 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     kotlin.run {
         while (true) {
             val (a, b) = readLine().split(' ').map { it.toInt() }
-            var ret = ' '
             if (a == 0 && b == 0) return@run
-            ret = if (a < b) {
-                if (b % a == 0) 'f'
-                else 'n'
-            } else {
-                if (a % b == 0) 'm'
-                else 'n'
-            }
-            when (ret) {
-                'f' -> w.write("factor\n")
-                'm' -> w.write("multiple\n")
-                'n' -> w.write("neither\n")
-            }
+            w.write(
+                if (b % a == 0) "factor"
+                else if (a % b == 0) "multiple"
+                else "neither"
+            )
+            w.newLine()
         }
     }
 
